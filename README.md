35225417ad41bd7133a08cff1926836a55d6f6a9
Ayumu Takai <atakai@hackwork.jp>
2022-11-05 16:08:04 +0900

    索引作成追加

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # vivliostyle-macro-samples
 
-Vivliostyle-CLI用自作マクロ集
+## Vivliostyle-CLI用自作マクロ集
 
+### HTMLヘッダへのJavascriptの挿入によるもの
+
+* vs-mermaid … mermaid.jsをレンダリングする
+* vs-image-notation … 画像に矢印などを描画する
+
+### VFMのReplace Fookによるもの
+
+* 索引作成
--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,14 +1,22 @@
+
+// 索引辞書
+const words = {};
+// 現在編集しているファイルのファイル名([begin:]で指定)
+let filename = "";
+
 // @ts-check
 /** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
 const vivliostyleConfig = {
-  title: 'マクロサンプル集', 
-  author: 'TAKAI Ayumu <atakai@hackwork.jp>', 
+  title: 'マクロサンプル集',
+  author: 'TAKAI Ayumu <atakai@hackwork.jp>',
   size: 'A4',
   theme: './style.css',
   image: 'ghcr.io/vivliostyle/cli:5.8.1',
-  entry: [ 
+  entry: [
     "mermaid.md",
-    "notation.md"
+    "notation.md",
+    "index_01.md",
+    "index.md"
   ], // 'entry' can be 'string' or 'object' if there's only single markdown file
   entryContext: './manuscripts',
   output: [ // path to generate draft file(s). default to '{title}.pdf'
@@ -22,20 +30,78 @@ const vivliostyleConfig = {
   // workspaceDir: '.vivliostyle', 
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
-  // vfm: { // options of VFM processor
-  //   replace: [ // specify replace handlers to modify HTML outputs
-  //     {
-  //       // This handler replaces {current_time} to a current local time tag.
-  //       test: /{current_time}/,
-  //       match: (_, h) => {
-  //         const currentTime = new Date().toLocaleString();
-  //         return h('time', { datetime: currentTime }, currentTime);
-  //       },
-  //     },
-  //   ],
-  //   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
-  //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
-  // },
-};
+  vfm: {
+    /* 索引作成関連 */
+    replace: [
+      {
+        // ファイル名の設定
+        test: /^\[begin:(.*?)\]/,
+        match: ([, fn], h) => {
+          // 現在処理中のファイル名を保持
+          filename = fn;
+          return null;
+        },
+      },
+      {
+        // [begin:]とそろえるため予約
+        test: /\[end\]/,
+        match: ([,], h) => {
+          return null;
+        },
+      },
+      {
+        // 索引の見出し語にしない [*!見出し語] | [*!見出し語,みだしご]
+        // 同ページに複数の見出し語があった場合に自動で集約できないため手作業で修正する
+        // タグを外してしまうと構成が変化した際に修正が大変なので!の付加で索引から除外できるようにしている
+        test: /\[\*!([^,]*?)(?:,(.*?))?\]/g,
+        match: ([, word, yomi], h) => {
+          return h("span", { class: "nhw" }, word);
+        },
+      },
+      {
+        // 索引の見出し語 [*見出し語] | [*見出し語,みだしご]
+        // Pythonの書籍で:が見出し語になっていたため,で区切る
+        test: /\[\*([^,]*?)(?:,(.*?))?\]/g,
+        match: ([, word, yomi], h) => {
+          yomi = yomi || word;
+          if (!words[word]) {
+            words[word] = [];
+          }
+          const id =
+            "word-" + Object.keys(words).length + "-" + words[word].length;
+          const href = filename.replace(/.md$/, ".html") + "#" + id;
+          words[word].push({ href, yomi });
+          return h("span", { id, class: "hw" }, word);
+        },
+      },
+      {
+        // 索引出力 [index:あ,い,う,え,お]
+        test: /\[index:(.*?)\]/,
+        match: ([, headwords], h) => {
+          headwords = headwords.split(",");
+          const dict = [];
+          for (const word of Object.keys(words)) {
+            if (headwords.includes(words[word][0].yomi.substring(0, 1))) {
+              dict[word] = words[word];
+            }
+          }
+          const divs = [];
+          for (const word of Object.keys(dict)) {
+            const l = [h("dt", word)];
+            for (const ref of dict[word]) {
+              l.push(h("dd", h("a", { href: ref.href }, ".")));
+            }
+            divs.push(h("div", l));
+          }
+          return h("dl", divs);
+        },
+      }
 
-module.exports = vivliostyleConfig;
+    ],
+    //   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
+    //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
+    }
+  };
+
+  module.exports = vivliostyleConfig;
+
